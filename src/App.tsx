e592ea8f4184394190569c991fbabd6d00665cc5
Mykola Romaniv <mykolaromaniw@gmail.com>
2020-12-25 12:10:31 +0200

    fix: links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-
 import { Switch, Route } from 'react-router-dom'
-
-import NavBar from './components/NavBar'
-
-import SignUp from './containers/Login/SignUp'
-
-import SignIn from './containers/Login/SignIn'
+import routes from 'constants/routes'
+import NavBar from 'components/NavBar'
+import SignUp from 'containers/Login/SignUp'
+import SignIn from 'containers/Login/SignIn'
+import Album from 'containers/Album'
 
 const App = (): JSX.Element => {
   return (
@@ -14,7 +12,8 @@ const App = (): JSX.Element => {
       <NavBar />
       <Switch>
         <Route path="/" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
+        <Route path={`/${routes.signIn}`} exact component={SignIn} />
+        <Route path={`/${routes.exercises}`} exact component={Album} />
       </Switch>
     </>
   )
--- src/containers/Login/SignIn/index.tsx
+++ src/containers/Login/SignIn/index.tsx
@@ -10,6 +10,7 @@ import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import { Link as RouterLink } from 'react-router-dom'
+import routes from 'constants/routes'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -67,7 +68,11 @@ const SignIn = (): JSX.Element => {
             id="password"
             autoComplete="current-password"
           />
-          <Link component={RouterLink} to="/test" variant="body2">
+          <Link
+            component={RouterLink}
+            to={`/${routes.exercises}`}
+            variant="body2"
+          >
             <Button
               type="submit"
               fullWidth
