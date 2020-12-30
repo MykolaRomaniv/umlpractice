9eb3478a32e57becf49dc500e8d3dff374002eda
Mykola Romaniv <mykolaromaniw@gmail.com>
2020-12-30 02:19:14 +0200

    feat(contants): add excerciseCategories

---
+++ src/app/constants/exerciseCategories.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-shadow
+enum exercisesCategories {
+  analytics = 'analytics',
+  designing = 'designing',
+  modeling = 'modeling',
+  constructing = 'constructing',
+  testing = 'testing',
+}
+
+export default exercisesCategories
