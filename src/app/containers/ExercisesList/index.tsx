5d0bed76364fa7b7e445faef6fea6e27fbbf6e0d
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-01-02 22:01:11 +0200

    refator(ExerciseList): use ExerciseItem

--- src/app/containers/ExercisesList/index.tsx
+++ src/app/containers/ExercisesList/index.tsx
@@ -3,17 +3,19 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Divider from '@material-ui/core/Divider'
-import Container from '@material-ui/core/Container'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import { Link as RouterLink } from 'react-router-dom'
-import Link from '@material-ui/core/Link'
+import Container from '@material-ui/core/Container'
 
-import ListItemLink from 'app/components/ListItemLink'
-import routes from 'app/constants/routes'
 import useQuery from 'app/hooks/useQuery'
 import exercisesCategories from 'app/constants/exerciseCategories'
+import ExerciseItem from './ExerciseItem'
 import useStyles from './styles'
+
+const exercises: IExerciseItem[] | undefined = [
+  { name: 'Завдання 2', deadline: 'До 15.01 12:00', type: 'Class' },
+  { name: 'Завдання 3', deadline: 'До 15.01 12:00', type: 'BPMN' },
+  { name: 'Завдання 4', deadline: 'До 15.01 12:00', type: 'Flow' },
+]
 
 const ExercisesList = (): JSX.Element => {
   const classes = useStyles()
@@ -21,145 +23,42 @@ const ExercisesList = (): JSX.Element => {
 
   return (
     <div className={classes.root}>
-      <Typography variant="h4" component="h2" className={classes.headers}>
-        {'Вправи'}
-      </Typography>
-      {categoryType === exercisesCategories.modeling && (
-        <List component="div">
-          <ListItemLink
-            button
-            className={classes.listItem}
-            to={`${routes.classExercise}`}
-          >
-            <Container className={classes.listItemTitle}>
-              {/* <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon> */}
-              <ListItemText primary="Завдання 2" />
-            </Container>
-            <Container className={classes.listItemTags}>
-              <Button
-                variant="outlined"
-                disableRipple
-                component="div"
-                className={classes.tag}
-              >
-                {'До 15.01 12:00'}
-              </Button>
-              <Button
-                variant="outlined"
-                className={classes.tag}
-                disableRipple
-                component="div"
-              >
-                {'Class'}
-              </Button>
-            </Container>
-            <Container className={classes.buttonContainer}>
-              <Link
-                component={RouterLink}
-                to={`/${routes.classExercise}`}
-                variant="body2"
-              >
-                <Button variant="contained" color="primary">
-                  {'Виконати'}
-                </Button>
-              </Link>
-            </Container>
-          </ListItemLink>
-          <ListItemLink
-            button
-            className={classes.listItem}
-            to={`${routes.bpmnExercise}`}
-          >
-            <Container className={classes.listItemTitle}>
-              {/* <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon> */}
-              <ListItemText primary="Завдання 2" />
-            </Container>
-            <Container className={classes.listItemTags}>
-              <Button
-                variant="outlined"
-                disableRipple
-                component="div"
-                className={classes.tag}
-              >
-                {'До 15.01 12:00'}
-              </Button>
-              <Button
-                variant="outlined"
-                className={classes.tag}
-                disableRipple
-                component="div"
-              >
-                {'BPMN'}
-              </Button>
-            </Container>
-            <Container className={classes.buttonContainer}>
-              <Link
-                component={RouterLink}
-                to={`/${routes.bpmnExercise}`}
-                variant="body2"
-              >
-                <Button variant="contained" color="primary">
-                  {'Виконати'}
-                </Button>
-              </Link>
-            </Container>
-          </ListItemLink>
-          <ListItemLink
-            button
-            className={classes.listItem}
-            to={`${routes.flowExercise}`}
-          >
-            <Container className={classes.listItemTitle}>
-              {/* <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon> */}
-              <ListItemText primary="Завдання 2" />
-            </Container>
-            <Container className={classes.listItemTags}>
-              <Button
-                variant="outlined"
-                disableRipple
-                component="div"
-                className={classes.tag}
-              >
-                {'До 15.01 12:00'}
-              </Button>
-              <Button
-                variant="outlined"
-                className={classes.tag}
-                disableRipple
-                component="div"
-              >
-                {'Flow'}
-              </Button>
-            </Container>
-            <Container className={classes.buttonContainer}>
-              <Link
-                component={RouterLink}
-                to={`/${routes.flowExercise}`}
-                variant="body2"
-              >
-                <Button variant="contained" color="primary">
-                  {'Виконати'}
-                </Button>
-              </Link>
-            </Container>
-          </ListItemLink>
-        </List>
+      {categoryType === exercisesCategories.modeling ? (
+        <>
+          <Typography variant="h4" component="h2" className={classes.headers}>
+            {'Вправи'}
+          </Typography>
+          <List component="div">
+            {exercises &&
+              exercises.map((item) => (
+                <ExerciseItem
+                  key={item.name}
+                  name={item.name}
+                  deadline={item.deadline}
+                  type={item.type}
+                />
+              ))}
+          </List>
+          <Divider className={classes.divider} />
+          <Typography variant="h4" component="h2" className={classes.headers}>
+            {'Зроблені\r'}
+          </Typography>
+          <List component="nav" aria-label="secondary mailbox folders">
+            <ListItem button>
+              <ListItemText primary="Завдання 1" />
+            </ListItem>
+          </List>
+        </>
+      ) : (
+        <Container className={classes.headerCenter}>
+          <Typography variant="h4" component="h2">
+            {'У вас поки немає завдань з цієї категорії\r'}
+          </Typography>
+          <Typography component="body">
+            {'Попросіть вчителя створити для вас'}
+          </Typography>
+        </Container>
       )}
-      <Divider className={classes.divider} />
-      <Typography variant="h4" component="h2" className={classes.headers}>
-        {'Зроблені\r'}
-      </Typography>
-      <List component="nav" aria-label="secondary mailbox folders">
-        <ListItem button>
-          <ListItemText primary="Завдання 1" />
-        </ListItem>
-      </List>
     </div>
   )
 }
--- src/app/containers/ExercisesList/styles.ts
+++ src/app/containers/ExercisesList/styles.ts
@@ -8,36 +8,16 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundColor: theme.palette.background.paper,
       margin: 'auto',
     },
-    listItem: {
-      display: 'flex',
-      alignItems: 'center',
-    },
-    listItemTitle: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      width: '35%',
-      paddingLeft: 0,
-    },
-    listItemTags: {
-      display: 'flex',
-      alignItems: 'center',
-    },
-    buttonContainer: {
-      textAlign: 'right',
-    },
-    tag: {
-      marginLeft: '20px',
-      '&:hover, &:focus': {
-        backgroundColor: 'inherit',
-      },
-    },
     divider: {
       marginTop: '10%',
     },
     headers: {
       paddingLeft: '10px',
       paddingTop: '15px',
+    },
+    headerCenter: {
+      textAlign: 'center',
+      backgroundColor: 'transparent',
     },
   }),
 )
