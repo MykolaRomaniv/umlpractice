fac64ad07d80f8198352d2e4fbfd072d18d7133e
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-01-12 01:06:45 +0200

    feat: add redux to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
 import './index.css'
 import App from 'app/App'
+import reducer from 'app/store/user'
 
 import reportWebVitals from './reportWebVitals'
 
+const rootReducer = combineReducers({
+  user: reducer,
+})
+
+export type RootState = ReturnType<typeof rootReducer>
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger)),
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
