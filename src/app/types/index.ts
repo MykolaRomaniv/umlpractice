29a746242d17d30e1216219affef1fe3a4e158e5
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-01-12 12:05:48 +0200

    fix: update types

---
+++ src/app/types/index.ts
@@ -0,0 +1,42 @@
+import { AxiosError } from 'axios'
+import { ThunkAction } from 'redux-thunk'
+import { Action } from 'redux'
+
+// eslint-disable-next-line import/no-cycle
+import { AllActionType } from 'app/store/action'
+// eslint-disable-next-line import/no-cycle
+import rootReducer from 'app/store'
+
+interface IBackendError {
+  data?: {
+    errors?: string | string[]
+  }
+}
+
+export type IError<T = { errors: string[] }> =
+  | {
+      name?: string
+      message?: string
+      stack?: string
+      code?: number
+    }
+  | string
+  | string[]
+  | AxiosError<T>
+  | IBackendError
+
+export type ReduxState = ReturnType<typeof rootReducer>
+
+export type AppThunkAsync<ReturnType = unknown> = ThunkAction<
+  Promise<ReturnType | undefined | void>,
+  ReduxState,
+  unknown,
+  Action<AllActionType>
+>
+
+export interface IUser {
+  type: 'student' | 'teacher'
+  id: number | string
+  name: string
+  email: string
+}
