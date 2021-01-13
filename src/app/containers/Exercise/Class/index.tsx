924f94d947ce476b7d6e418e9d263636c8ba03ee
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-01-13 19:06:21 +0200

    feat: done basics for class diagram

--- src/app/containers/Exercise/Class/index.tsx
+++ src/app/containers/Exercise/Class/index.tsx
@@ -15,11 +15,13 @@ import {
   UmlEnumerationMemberModel,
   TextStyleModel,
   ShapeStyleModel,
-  // ClassifierShape,
-  // AssociationFlow,
+  AssociationFlow,
+  Multiplicity,
+  ClassifierShape,
 } from '@syncfusion/ej2-react-diagrams'
 
 import SampleBase from '../sample-base'
+import styles from './styles.module.scss'
 
 let diagramInstance: DiagramComponent | null
 
@@ -27,11 +29,11 @@ let diagramInstance: DiagramComponent | null
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function createConnector(
+const createConnector = (
   id: string,
   sourceID: string,
   targetID: string,
-): ConnectorModel {
+): ConnectorModel => {
   const connector: ConnectorModel = {}
   connector.id = id
   connector.sourceID = sourceID
@@ -39,15 +41,34 @@ function createConnector(
   return connector
 }
 
+const createConnection = (
+  id: string,
+  relationship: ClassifierShape,
+  associationType?: AssociationFlow,
+  multiplicityType?: Multiplicity,
+): ConnectorModel => ({
+  id,
+  // Define connector start and end points
+  sourcePoint: { x: 100, y: 100 },
+  targetPoint: { x: 200, y: 200 },
+  type: 'Straight',
+  shape: {
+    type: 'UmlClassifier',
+    relationship,
+    // Define type of association
+    // association: 'Directional',
+    associationType,
+    multiplicity: {
+      type: multiplicityType,
+    },
+  },
+})
+
 // create class Property
-const createProperty = (name: string, type: string): object => {
-  return { name, type }
-}
+const createProperty = (name: string, type: string): object => ({ name, type })
 
 // create class Methods
-const createMethods = (name: string, type: string): object => {
-  return { name, type }
-}
+const createMethods = (name: string, type: string): object => ({ name, type })
 
 const createEnumMember = (
   name: string,
@@ -147,61 +168,135 @@ export default class UMLClassDiagram extends SampleBase {
   render() {
     return (
       <div className="control-section">
-        <button
-          id="class"
-          aria-label="Btn"
-          type="button"
-          onClick={() => {
-            diagramInstance?.add(createClass(Date(), 100, 100, 'ClassName'))
-          }}
-        />
-        <button
-          id="interface"
-          aria-label="Btn"
-          type="button"
-          onClick={() => {
-            diagramInstance?.add(
-              createInterface(Date(), 100, 100, 'InterfaceName'),
-            )
-          }}
-        />
-        <button
-          id="enumeration"
-          aria-label="Btn"
-          type="button"
-          onClick={() => {
-            diagramInstance?.add(
-              createEnumeration(Date(), 100, 100, 'EnumName'),
-            )
-          }}
-        />
-        <DiagramComponent
-          id="diagram"
-          width="100%"
-          height="800px"
-          ref={(diagram) => (diagramInstance = diagram)}
-          connectors={connectors}
-          // Sets the default values of a node
-          getNodeDefaults={(obj: NodeModel) => {
-            obj.style = { fill: '#26A0DA', strokeColor: 'white' }
-            return obj
-          }}
-          // Sets the default values of a connector
-          getConnectorDefaults={(connector: ConnectorModel) => {
-            return connector
-          }}
-          // set an label style for nodes
-          setNodeTemplate={(node: NodeModel) => {
-            if (node.annotations && node.annotations.length > 0) {
-              for (let i = 0; i < node.annotations.length; i++) {
-                const annotation: ShapeAnnotationModel = node.annotations[i]
-                if (annotation && annotation.style) {
-                  annotation.style.color = 'white'
+        <div className={styles.contentWrapper}>
+          <div className={styles.palette}>
+            <div className={styles.paletteSection}>
+              <button
+                id="class"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(
+                    createClass(Date(), 100, 100, 'ClassName'),
+                  )
+                }}
+              >
+                <div>{'Class'}</div>
+              </button>
+              <button
+                id="interface"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(
+                    createInterface(Date(), 100, 100, 'InterfaceName'),
+                  )
+                }}
+              >
+                <div>{'Interface'}</div>
+              </button>
+              <button
+                id="enumeration"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(
+                    createEnumeration(Date(), 100, 100, 'EnumName'),
+                  )
+                }}
+              >
+                <div>{'Enum'}</div>
+              </button>
+            </div>
+            <div className={styles.paletteSection}>
+              <button
+                id="association"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(createConnection(Date(), 'Association'))
+                }}
+              >
+                <div>{'Association'}</div>
+              </button>
+              <button
+                id="aggregation"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(createConnection(Date(), 'Aggregation'))
+                }}
+              >
+                <div>{'Aggregation'}</div>
+              </button>
+              <button
+                id="composition"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(createConnection(Date(), 'Composition'))
+                }}
+              >
+                <div>{'Composition'}</div>
+              </button>
+              <button
+                id="dependency"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(createConnection(Date(), 'Dependency'))
+                }}
+              >
+                <div>{'Dependency'}</div>
+              </button>
+              <button
+                id="inheritance"
+                aria-label="Btn"
+                type="button"
+                className={styles.btn}
+                onClick={() => {
+                  diagramInstance?.add(createConnection(Date(), 'Inheritance'))
+                }}
+              >
+                <div>{'Inheritance'}</div>
+              </button>
+            </div>
+          </div>
+          <DiagramComponent
+            id="diagram"
+            width="85%"
+            height="700px"
+            ref={(diagram) => (diagramInstance = diagram)}
+            connectors={connectors}
+            // Sets the default values of a node
+            getNodeDefaults={(obj: NodeModel) => {
+              obj.style = { fill: '#26A0DA', strokeColor: 'white' }
+              return obj
+            }}
+            // Sets the default values of a connector
+            getConnectorDefaults={(connector: ConnectorModel) => {
+              return connector
+            }}
+            // set an label style for nodes
+            setNodeTemplate={(node: NodeModel) => {
+              if (node.annotations && node.annotations.length > 0) {
+                for (let i = 0; i < node.annotations.length; i++) {
+                  const annotation: ShapeAnnotationModel = node.annotations[i]
+                  if (annotation && annotation.style) {
+                    annotation.style.color = 'white'
+                  }
                 }
               }
-            }
-          }}
-        />
+            }}
+          />
+        </div>
       </div>
     )
   }
