ab01335242d20b9d4ca0b3c689b356d9e8a92a69
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-01-13 22:42:05 +0200

    feat(RadioGroup): add onChange prop

--- src/app/components/RadioGroup/index.tsx
+++ src/app/components/RadioGroup/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+/* eslint-disable react/require-default-props */
+import React, { useEffect } from 'react'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
 
-const RadioButtonsGroup = (): JSX.Element => {
-  const [value, setValue] = React.useState('student')
+import { IUserType } from 'app/types'
+
+interface IProps {
+  onChange?: (value: IUserType) => void
+}
+
+const RadioButtonsGroup = ({ onChange }: IProps): JSX.Element => {
+  const [value, setValue] = React.useState<IUserType>('student')
+
+  useEffect(() => {
+    onChange?.(value)
+  }, [onChange, value])
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue((event.target as HTMLInputElement).value)
+    setValue((event.target as HTMLInputElement).value as IUserType)
   }
 
   return (
