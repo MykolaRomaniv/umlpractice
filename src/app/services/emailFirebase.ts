1f79c9b00aa14fdaedab650693341f51317536ca
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-01-13 22:36:06 +0200

    feat(services): add email firebase sign in/sign up

---
+++ src/app/services/emailFirebase.ts
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+import firebase from './firebaseApi'
+
+export const signInWithEmailPassword = (
+  email: string,
+  password: string,
+): void => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log('user', user)
+    })
+    .catch((error) => {
+      console.log('error', error)
+      const errorCode = error.code
+      console.log('errorCode', errorCode)
+      const errorMessage = error.message
+      console.log('errorMessage', errorMessage)
+    })
+}
+
+export const signUpWithEmailPassword = (
+  email: string,
+  password: string,
+): void => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log('user', user)
+    })
+    .catch((error) => {
+      console.log('error', error)
+      const errorCode = error.code
+      console.log('errorCode', errorCode)
+      const errorMessage = error.message
+      console.log('errorMessage', errorMessage)
+    })
+}
+
+export const sendEmailVerification = (): void => {
+  firebase
+    .auth()
+    ?.currentUser?.sendEmailVerification()
+    .then(() => {
+      console.log('Email verification sent!')
+    })
+}
+
+export const sendPasswordReset = (email: string): void => {
+  firebase
+    .auth()
+    .sendPasswordResetEmail(email)
+    .then(() => {
+      console.log('Password reset email sent!')
+    })
+    .catch((error) => {
+      console.log('error', error)
+      const errorCode = error.code
+      console.log('errorCode', errorCode)
+      const errorMessage = error.message
+      console.log('errorMessage', errorMessage)
+    })
+}
+
+export default {
+  signInWithEmailPassword,
+  signUpWithEmailPassword,
+  sendEmailVerification,
+  sendPasswordReset,
+}
