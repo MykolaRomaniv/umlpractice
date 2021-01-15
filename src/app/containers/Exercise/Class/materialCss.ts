3f6a3d2718668de460d4a08c275aa4c0610250b2
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-01-15 13:33:39 +0200

    feat(class exercise): add more styles

---
+++ src/app/containers/Exercise/Class/materialCss.ts
@@ -0,0 +1,13 @@
+import { createStyles } from '@material-ui/core/styles'
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const useStyles = () =>
+  createStyles({
+    points: {
+      marginTop: '10px',
+      textAlign: 'center',
+      fontSize: '32px',
+    },
+  })
+
+export default useStyles
